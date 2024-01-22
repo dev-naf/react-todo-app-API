ca0abe796f351116a5d145568a190daf1cee5168
dev-naf <ahnaf.fattah@outlook.com>
2024-01-22 14:20:48 +0700

    Update mariadb conf

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 let mysql = require('mysql')
 
 let connection = mysql.createConnection({
-    host: 'localhost',
+    host: '192.168.1.100',
+    port: 3306,
     user: 'admin',
     password: '123',
     database : 'Todo-List-Dummy-API'
